4a8e6bde64565ab558bc79342bd5fda01442054f
sevenelevon <93984090+sevenelevon@users.noreply.github.com>
2022-12-16 23:25:18 +0300

    test3

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,11 +3,7 @@ import React from "react";
 function Header() {
   return (
     <>
-    <h1>Введение test</h1>
-    <div className="image_site">
-        <img src="https://static.promopult.ru/uploads/library/img/home-page_img_2.png" alt="show"></img>
-    </div>
-    <h1>Введение test</h1>
+    <h1>Результат выполнения после пуша в github и автоматического</h1>
     </>
   );
 }
