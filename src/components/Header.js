71dc3ace7e755553db4f00028293f478b044fecc
sevenelevon <93984090+sevenelevon@users.noreply.github.com>
2022-12-16 17:18:09 +0300

    create

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+import logo from "../logo.svg";
+
+function Header() {
+  return (
+    <>
+    <h1>Введение test</h1>
+    <div className="image_site">
+        <img src="https://static.promopult.ru/uploads/library/img/home-page_img_2.png"></img>
+    </div>
+    </>
+  );
+}
+// Add CSS styles for the header
+
+
+export default Header;
